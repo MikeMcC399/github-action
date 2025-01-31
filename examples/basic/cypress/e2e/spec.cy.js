f993d0d680b6aaca63eb2dee2c0737bd3ca3ecf3
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2025-01-31 16:36:39 +0100

    add bad cy.origin test

--- examples/basic/cypress/e2e/spec.cy.js
+++ examples/basic/cypress/e2e/spec.cy.js
@@ -1,6 +1,7 @@
-it('works', () => {
-  expect(42).to.equal(21 + 21)
-  cy.visit('https://example.cypress.io').then(() => {
-    expect('hello').to.equal('hello')
+describe('testing cy.origin()', () => {
+  it('navigates to two different superdomains', () => {
+    cy.visit('https://github.com/cypress-io/cypress')
+    cy.visit('https://example.cypress.io') // visit a different superdomain
+    cy.get('h1').should('contain', 'Kitchen Sink') // fails
   })
 })
