12472e42aa0e546c075b54c3cb44f6c880d0c430
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2025-02-28 12:23:16 +0100

    run in arm

--- examples/basic/cypress/e2e/spec.cy.js
+++ examples/basic/cypress/e2e/spec.cy.js
@@ -4,3 +4,9 @@ it('works', () => {
     expect('hello').to.equal('hello')
   })
 })
+it('works', () => {
+  expect(40).to.equal(20 + 20)
+  cy.visit('https://example.cypress.io').then(() => {
+    expect('hello').to.equal('hello')
+  })
+})
