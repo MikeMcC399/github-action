661d0d507b778f6d6cd46919c14ac31d7c3465b7
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2025-03-18 23:08:53 +0100

    remove setupnodeevents

--- examples/browser/cypress.config.js
+++ examples/browser/cypress.config.js
@@ -4,28 +4,28 @@ const os = require('os')
 module.exports = defineConfig({
   fixturesFolder: false,
   e2e: {
-    setupNodeEvents(on) {
-      on('before:browser:launch', (browser, launchOptions) => {
-        console.log('before launching browser')
-        console.log(browser)
+    // setupNodeEvents(on) {
+    //   on('before:browser:launch', (browser, launchOptions) => {
+    //     console.log('before launching browser')
+    //     console.log(browser)
 
-        if (browser.name === 'chrome') {
-          // https://www.ghacks.net/2013/10/06/list-useful-google-chrome-command-line-switches/
-          launchOptions.args.push('--window-size=1920,1080')
+    //     if (browser.name === 'chrome') {
+    //       // https://www.ghacks.net/2013/10/06/list-useful-google-chrome-command-line-switches/
+    //       launchOptions.args.push('--window-size=1920,1080')
 
-          console.log('chrome launch args:')
-          console.log(launchOptions.args.join(os.EOL))
-          return launchOptions
-        }
-      })
+    //       console.log('chrome launch args:')
+    //       console.log(launchOptions.args.join(os.EOL))
+    //       return launchOptions
+    //     }
+    //   })
 
-      on('task', {
-        log(message) {
-          console.log(message)
-          return null
-        },
-      })
-    },
+    //   on('task', {
+    //     log(message) {
+    //       console.log(message)
+    //       return null
+    //     },
+    //   })
+    // },
     supportFile: false,
   },
 })
