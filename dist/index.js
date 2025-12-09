41daa369abb37ac9729def4cd652968af59a4b1c
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2025-12-09 10:37:29 +0100

    implement package manager cache skip

--- dist/index.js
+++ dist/index.js
@@ -94238,8 +94238,16 @@ const installMaybe = () => {
     return Promise.resolve()
   }
 
+  const packageManagerCacheEnabled = getInputBool(
+    'package-manager-cache',
+    true
+  )
+  const npmCachePromise = packageManagerCacheEnabled
+    ? restoreCachedNpm()
+    : Promise.resolve(false)
+
   return Promise.all([
-    restoreCachedNpm(),
+    npmCachePromise,
     restoreCachedCypressBinary()
   ]).then(([npmCacheHit, cypressCacheHit]) => {
     debug(`npm cache hit ${npmCacheHit}`)
@@ -94254,8 +94262,11 @@ const installMaybe = () => {
         }
 
         debug('verifying Cypress binary')
+        const saveNpmPromise = packageManagerCacheEnabled
+          ? saveCachedNpm()
+          : Promise.resolve(undefined)
         return verifyCypressBinary()
-          .then(saveCachedNpm)
+          .then(() => saveNpmPromise)
           .then(saveCachedCypressBinary)
       })
     })
